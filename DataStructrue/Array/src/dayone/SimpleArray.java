cfa0cefdfd4078e54c919e7626b6facdb326a02b
mike <codescxw@outlook.com>
2018-06-06 18:34:18 +0800

    Array(dayone)
    test github and intelliJ enviroment

---
+++ DataStructrue/Array/src/dayone/SimpleArray.java
@@ -0,0 +1,10 @@
+package dayone;
+
+public class SimpleArray {
+    public static void main(String[] args){
+        int[] arr = new int[]{3,2,1};
+        for (int i = 0; i < 3; i++) {
+            System.out.println("arr[" + i + "]: " + arr[i]);
+        }
+    }
+}
